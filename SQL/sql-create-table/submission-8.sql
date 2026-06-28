35acf7ac4b078480bce5fb490a2c40adb4cdc7ae
Harshal Thakur <132253679+harshal1223@users.noreply.github.com>
2026-06-28 21:39:28 +0530

    Add: sql-create-table - submission-8

---
+++ SQL/sql-create-table/submission-8.sql
@@ -0,0 +1,18 @@
+Create Table videos(
+    id integer ,
+    name Text,
+    created_at date,
+    published Boolean
+);
+
+
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
