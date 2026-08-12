0a31c1d32f4d2afd05b2933e991c378462e9310d
Harshal Thakur <132253679+harshal1223@users.noreply.github.com>
2026-08-12 19:49:04 +0530

    Add: sql-drop-table - submission-2

---
+++ SQL/sql-drop-table/submission-2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE books (
+  id INTEGER,
+  title TEXT,
+  author TEXT
+);
+-- Do not modify above this line --
+
+Alter table books Add column published_year Integer;
+Alter Table books rename column id to isbn;
+alter table books drop column author;
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, column_default
+FROM information_schema.columns
+WHERE table_name = 'books'
+ORDER BY column_name;
